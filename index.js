576ec2b73bf348953bdcc5db90c4b4eee27a5c09
John Bintz <john@coswellproductions.com>
2016-03-31 14:36:51 -0400

    [only-proxy-mode] Enable only proxy mode for when Selenium is elsewhere

--- index.js
+++ index.js
@@ -1,89 +1,30 @@
 var selenium = require('selenium-standalone');
-var proxy = require('express-http-proxy');
 var Request = require('request');
+var minimist = require('minimist');
 
-var app = require('express')();
-var currentSession = null;
-var currentSessionObj = null;
-var seleniumPort = 4444;
+var arg = minimist(
+  process.argv.slice(2),
+  { boolean: ['only-proxy'] }
+);
 
-var initBrowser = function(cb) {
-  Request({
-    method: 'post',
-    url: 'http://localhost:' + seleniumPort + '/wd/hub/session/' + currentSession + '/url',
-    json: { url: "data:text/html;charset=utf-8;base64,PGh0bWw+PGhlYWQ+PHRpdGxlPm5vZGUtcGVyc2lzdGVudF9zZWxlbml1bTwvdGl0bGU+PGhlYWQ+PGJvZHk+PGgxPm5vZGUtcGVyc2lzdGVudF9zZWxlbml1bSBzdGFydGluZy4uLjwvaDE+PC9ib2R5PjwvaHRtbD4=" }
-  }, function(err, response, body) {
-    cb();
-  });
+var app = require('./proxy');
+app.config = arg;
+
+function run() {
+  console.log("Persistent selenium listening on port 4443");
+  app.listen(arg['app-port'] || 4443);
 };
 
-app.use(proxy('localhost', {
-  filter: function(req, res) {
-    if (currentSession) {
-      if (req.path === '/wd/hub/session/' + currentSession &&
-        req.method.toLowerCase() === 'delete') {
-        req._wasDelete = true;
-      }
-
-      if (req.path === '/wd/hub/session' &&
-        req.method.toLowerCase() === 'post') {
-        req._wasCreate = true;
-      }
-    }
-
-    return true;
-  },
-  decorateRequest: function(req) {
-    req.port = seleniumPort;
-
-    if (currentSession) {
-      if (req.path === '/wd/hub/session/' + currentSession &&
-        req.method.toLowerCase() === 'delete') {
-        req.method = 'GET';
-        req.path = '/wd/hub/sessions';
-      }
-
-      if (req.path === '/wd/hub/session' &&
-        req.method.toLowerCase() === 'post') {
-        req.method = 'GET';
-        req.path = '/wd/hub/sessions';
-      }
-    }
-
-    return req;
-  },
-  intercept: function(origRes, data, req, res, callback) {
-    if (!currentSession && req.url === '/wd/hub/session' &&
-      req.method.toLowerCase() === 'post') {
-
-      currentSessionObj = JSON.parse(data.toString('utf-8'));
-      currentSession = currentSessionObj.sessionId;
-      initBrowser(function() { callback(null, data); });
-    } else {
-      if (req._wasCreate) {
-        data = JSON.stringify(currentSessionObj);
-        initBrowser(function() { callback(null, data); });
+if (arg['only-proxy']) {
+  run();
+} else {
+  selenium.install({ logger: function(msg) { console.log(msg); }}, function(instErr) {
+    selenium.start({ spawnOptions: {}, seleniumArgs: arg._ }, function(startErr) {
+      if (startErr) {
+        console.log(startErr);
       } else {
-        if (req._wasDelete) {
-          callback(null, JSON.stringify('{}'));
-        } else {
-          callback(null, data);
-        }
+        run();
       }
-    }
-  }
-}));
-
-var seleniumArgs = process.argv.slice(2);
-
-selenium.install({ logger: function(msg) { console.log(msg); }}, function(err) {
-  selenium.start({ spawnOptions: {}, seleniumArgs: seleniumArgs }, function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Persistent selenium listening on port 4443");
-      app.listen(4443);
-    }
+    });
   });
-});
-
+}
---
+++ proxy.js
@@ -0,0 +1,77 @@
+var app = require('express')();
+var proxy = require('express-http-proxy');
+
+var currentSession = null;
+var currentSessionObj = null;
+
+function initBrowser(cb) {
+  var seleniumPort = app.config['selenium-port'] || 4444;
+  var seleniumHost = app.config['selenium-host'] || 'localhost';
+
+  Request({
+    method: 'post',
+    url: 'http://' + seleniumHost + ':' + seleniumPort + '/wd/hub/session/' + currentSession + '/url',
+    json: { url: "data:text/html;charset=utf-8;base64,PGh0bWw+PGhlYWQ+PHRpdGxlPm5vZGUtcGVyc2lzdGVudF9zZWxlbml1bTwvdGl0bGU+PGhlYWQ+PGJvZHk+PGgxPm5vZGUtcGVyc2lzdGVudF9zZWxlbml1bSBzdGFydGluZy4uLjwvaDE+PC9ib2R5PjwvaHRtbD4=" }
+  }, function(err, response, body) {
+    cb();
+  });
+};
+
+app.use(proxy('localhost', {
+  filter: function(req, res) {
+    if (currentSession) {
+      if (req.path === '/wd/hub/session/' + currentSession &&
+        req.method.toLowerCase() === 'delete') {
+        req._wasDelete = true;
+      }
+
+      if (req.path === '/wd/hub/session' &&
+        req.method.toLowerCase() === 'post') {
+        req._wasCreate = true;
+      }
+    }
+
+    return true;
+  },
+  decorateRequest: function(req) {
+    req.port = app.config['selenium-port'] || 4444;
+
+    if (currentSession) {
+      if (req.path === '/wd/hub/session/' + currentSession &&
+        req.method.toLowerCase() === 'delete') {
+        req.method = 'GET';
+        req.path = '/wd/hub/sessions';
+      }
+
+      if (req.path === '/wd/hub/session' &&
+        req.method.toLowerCase() === 'post') {
+        req.method = 'GET';
+        req.path = '/wd/hub/sessions';
+      }
+    }
+
+    return req;
+  },
+  intercept: function(origRes, data, req, res, callback) {
+    if (!currentSession && req.url === '/wd/hub/session' &&
+      req.method.toLowerCase() === 'post') {
+
+      currentSessionObj = JSON.parse(data.toString('utf-8'));
+      currentSession = currentSessionObj.sessionId;
+      initBrowser(function() { callback(null, data); });
+    } else {
+      if (req._wasCreate) {
+        data = JSON.stringify(currentSessionObj);
+        initBrowser(function() { callback(null, data); });
+      } else {
+        if (req._wasDelete) {
+          callback(null, JSON.stringify('{}'));
+        } else {
+          callback(null, data);
+        }
+      }
+    }
+  }
+}));
+
+module.exports = app;
